45aa7930e2d4f7175de4fd84974dae79a4f0b77c
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2009-06-01 02:36:34 +0000

    add manual check

---
+++ Gnome/src/MyManualTestMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author xzhou
+ *
+ */
+public class MyManualTestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ManualVerify mv = new ManualVerify();
+		mv.testFalseNegative();
+	}
+
+}
