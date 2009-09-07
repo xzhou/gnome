bcb98fe8af0812dcfbb4923c81be6fd5562a6405
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2009-09-07 23:57:16 +0000

    change directory for porting the program to tank

--- hBlockRecombo/HAPMAP77test.m
+++ hBlockRecombo/HAPMAP77test.m
@@ -2,8 +2,11 @@
 %markov chain model and generate multiple haplotype sequence for
 %experiments
 
-cd '..';
-cd 'HAPMAP';
+cd '/home/xzhou/research_linux/gnome/workspace/data/HAPMAP'
+
+%for each pair of blocks, we repeat the experiments for repeatTimes to get
+%a better result.
+repeatTimes = 100;
 
 %ceuSequence = readSeq4('chr10_FGFR2_200kb_phased_CEU.fasta');
 ceuSeqStruct = fastaread('chr10_FGFR2_200kb_phased_CEU.fasta');
@@ -43,12 +46,12 @@ yriR = calcR(yriSeq4, alleleMapping);
 % saveas(gcf, 'ceuR_yriR.pdf');
 
 %%cut the graph into 3 parts for easy process
-% absCeuR = abs(ceuR);
-% absYriR = abs(yriR);
-% plotWithSignSimple(absCeuR(1:80, 1:80), absYriR(1:80,1:80), 0, 0);
-% xlabel('yri 1:80');
-% ylabel('ceu 1:80');
-% saveas(gcf, 'ceuR_yriR 1_80.pdf');
+absCeuR = ceuR;
+absYriR = yriR;
+plotWithSignSimple(absCeuR(1:80, 1:80), absYriR(1:80,1:80), 0, 0);
+xlabel('yri 1:80');
+ylabel('ceu 1:80');
+saveas(gcf, 'ceuR_yriR 1_80.pdf');
 % 
 % plotWithSignSimple(absCeuR(50:120, 50:120), absYriR(50:120, 50:120), 0, 0);
 % xlabel('yri 50:120');
@@ -60,10 +63,25 @@ yriR = calcR(yriSeq4, alleleMapping);
 % ylabel('ceu 90:end');
 % saveas(gcf, 'ceuR_yriR 90_end.pdf');
 
+
+
+%start matlab pool for parallel compuation
+isOpen = matlabpool('size') > 0;
+if ~isOpen
+    matlabpool 2;
+end
+
 %identifiy blocks
 blocks = [1 10; 12 23; 26 44];
 
 %%learn first block
+% [targetR refSeq blockAlleleMapping] = getTarget(ceuR, yriSeq4, alleleMapping, blocks(1:2,:));
+% parfor i = 1:20
+%     [finalSeq finalR finalSignRate] = hbOneBlockRecombination(targetR, refSeq, blockAlleleMapping, blocks(1:2,:));
+% end
+
+%do the experiments 20 times
+blocks = [1 23; 25 46];
 [targetR refSeq blockAlleleMapping] = getTarget(ceuR, yriSeq4, alleleMapping, blocks(1:2,:));
 parfor i = 1:20
     [finalSeq finalR finalSignRate] = hbOneBlockRecombination(targetR, refSeq, blockAlleleMapping, blocks(1:2,:));
