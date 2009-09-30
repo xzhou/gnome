6acece5d3387205a518b7045c6d95b2b88599365
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2009-09-30 14:20:34 +0000

    add getBlockFreq.m

---
+++ hBlockRecombo/getBlockFreq.m
@@ -0,0 +1,14 @@
+function [nRecombo] = getBlockFreq(currentSeq, newBlock)
+    [m n] = size(currentSeq)
+    
+    a = newBlock(1, 1);
+    b = newBlock(1, 2);
+    
+    block = currentSeq(:,a:b);
+    
+    [uniqueBLock, 
+    
+    
+    
+    
+end
