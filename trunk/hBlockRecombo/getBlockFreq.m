36e3b5ffc324e821b19f84eb99a2acc959a4e49b
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2009-09-30 14:45:29 +0000

    finish getBlockFreq

--- trunk/hBlockRecombo/getBlockFreq.m
+++ trunk/hBlockRecombo/getBlockFreq.m
@@ -1,14 +1,13 @@
-function [nRecombo] = getBlockFreq(currentSeq, newBlock)
-    [m n] = size(currentSeq)
+function [uniqueBLock, freq] = getBlockFreq(currentSeq, newBlock)
+    [m n] = size(currentSeq);
     
     a = newBlock(1, 1);
     b = newBlock(1, 2);
     
     block = currentSeq(:,a:b);
     
-    [uniqueBLock, 
-    
-    
-    
+    [uniqueBLock I J]  = unique(block, 'rows');
+	
+	freq = accumarray(J, 1);
     
 end
