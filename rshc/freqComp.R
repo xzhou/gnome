ba0fe39e4dd28d36e555d42c17485059fddb0dff
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2009-11-03 04:54:54 +0000

    add freqComp.R

--- rshc/freqComp.R
+++ rshc/freqComp.R
@@ -1,6 +1,41 @@
+#convert to genotype sequence
+genotypeEncode <- function(genotype)
+  {
+    m <- nrow(genotype)
+    n <- ncol(genotype)
+    if(n %% 2 == 1)
+      {
+        warning("ncol is a odd number")
+        stop("can not encode genotype")
+      }
+    nSnps <- n/2
+
+    codes = matrix(m, nSnps)
+
+    for(i in seq(1, m))
+      for(j in seq(1,nSnps))
+        {
+          val = 0;
+          a = genotype[i, 2*j]
+          b = genotype[i, 2*j+1]
+          if(a == b == 1)
+            val = 0
+          else if (a == 1 && b == 2 || a == 2 && b == 1)
+            val = 1
+          else
+            val = 2
+
+         codes[i,j] = val
+        }
+    codes
+  }
+
+
+
 #function get block frequency
 getBlockFreq <- function(genotype)
   {
+    
   }
 
 
