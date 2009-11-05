aad0ef57f8fbc2ba7424e0ae8a43ac582b2c1fe2
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2009-11-05 03:48:38 +0000

    add

---
+++ genotypeRecomb/genotypeRecombo.m
@@ -0,0 +1,6 @@
+%genotype recombination will break the genotype into blocks and use inner
+%block learning and inter block learning to learn the sign
+%% 
+function [] =  genotypeRecombo(targetRs, refGSeq, blocks)
+    
+end
---
+++ genotypeRecomb/randomSelectGenotype.m
@@ -0,0 +1,4 @@
+%this function divide the genotype sequence into two groups
+function [caseGenotype, refGenotype] = randomSelectGenotype(genotype)
+    
+end
