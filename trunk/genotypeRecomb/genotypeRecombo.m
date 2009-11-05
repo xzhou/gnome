64fb65e01f8a815c82008ec1b8f7153bab514fe9
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2009-11-05 04:44:01 +0000

    complete genotype learning framework

--- trunk/genotypeRecomb/genotypeRecombo.m
+++ trunk/genotypeRecomb/genotypeRecombo.m
@@ -1,6 +1,7 @@
 %genotype recombination will break the genotype into blocks and use inner
 %block learning and inter block learning to learn the sign
+
 %% 
 function [] =  genotypeRecombo(targetRs, refGSeq, blocks)
-    
+    %reading
 end
---
+++ trunk/genotypeRecomb/recombMain.m
@@ -0,0 +1,24 @@
+%this is the main function
+function [] = recombMain()
+    %% add path
+    try
+        addpath '/home/xzhou/research_linux/gnome/workspace/hBlockRecombo'
+        addpath '/home/xzhou/research_linux/gnome/workspace/MyDrTest'
+        addpath '/home/xzhou/research_linux/gnome/workspace/genotypeRecomb'
+    catch exception
+        %do nothing
+    end
+    
+    %% cd to data
+    try
+        cd 'D:\IUBResearch\Projects\Bioinfor\data\HAPMAP';
+        disp 'WINDOWS'
+    catch e
+        cd '/home/xzhou/research_linux/gnome/workspace/data/HAPMAP';
+        disp 'LINUX'
+    end
+    
+    fastaFile = 'hapmap_chr7_80SNP_CEU_haplotype.fasta';
+    genotypeAll = genotypeHelpFuncs.readGenotypeFromFasta(fastaFile);
+    
+end
