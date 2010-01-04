d48758f9499d14aaddad2181c526175285bb3f41
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2010-01-04 18:39:13 +0000

    update

--- trunk/genotypeRecomb/genotypeInterBlockRecomb.m
+++ trunk/genotypeRecomb/genotypeInterBlockRecomb.m
@@ -1,30 +1,31 @@
 function [result] = genotypeInterBlockRecomb(caseSeq, startingSeq, blocks, alleleMapping, false)
-  %recombination of genotype sequence
-  [nBlocks tmp] = size(blocks);
-  if tmp <= 2
+    %recombination of genotype sequence
+    [nBlocks tmp] = size(blocks);
+    if tmp <= 2
       %extend the block to pre calculate the size
       blocks = [blocks blocks(:,2) - blocks(:,1)]
-  end
-  
-  
-  [nIndividual nSnps] = size(startingSeq);
-  
-  initR = estimateR(startingSeq, alleleMapping);
-  
-  trials = 1000;
-  result = zeros(nSnps, nSnps, trials);
-  
-  
-  
-  
-    parfor itr = 1:trials
+    end
+
+    %==============config==================
+    trials = 1000;
+    nInterBlockRecomb = 1000;
+    %==============end config==============
+    
+    [nIndividual nSnps] = size(startingSeq);
+
+    initR = estimateR(startingSeq, alleleMapping);
+
+    result = zeros(nSnps, nSnps, trials);
+    
+    for itr = 1:trials
         for i = 1:(nBlocks-1)
             for j = (i+1):nBlocks
                 block1 = blocks(i,:);
                 block2 = blocks(j,:);
                 if block1(1,3) >= block2(1,3)
-                    
+                    for 
                 else
+
                 end
             end
         end
