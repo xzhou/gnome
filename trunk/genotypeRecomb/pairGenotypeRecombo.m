08f3f55105316ea0e49ec03f3790d760440e6495
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2010-01-05 18:13:40 +0000

    add genotype eval function

---
+++ trunk/genotypeRecomb/pairGenotypeRecombo.m
@@ -0,0 +1,53 @@
+function [finalSeq finalR finalSignRate finalQual blockMask] = pairGenotypeRecombo(targetR, caseSeq, startingSeq, block1, block2, alleleMapping, alpha)
+    %function pairGenotypeRecombo recombine sequences
+    %======= config =========
+    expT = numel(targetR)/2*1e-10;
+    maxIT = 1e4;
+    smallFilter = 0.01;
+    %======= end config =====
+    
+    verbose = true;
+    targetRs = targetR.*targetR;
+    
+    %initialize
+    [currentQuality currentR] = evalGenotypeSeq(targetRs, startingSeq, alleleMapping, block1, block2, alleleMapping, alpha, smallFilter);
+    initSignRate = blockSignRate(targetR, currentR, block1, block2);
+    initQuality = currentQuality;
+    
+    itr = 0;
+    signRate = initSignRate;
+    currentSeq = startingSeq;
+    while t < maxIT
+        t = t + 1;
+        newSampelSeq = newMutate(currentSeq, block2);
+        [newQuality newR blockMask] = evalGenotypeSeq(targetRs, currentSeq, alleleMapping, block1, block2, alleleMapping, alpha, smallFilter);
+        Qdiff = newQuality - currentQuality;
+        if Qdiff < 0
+            p = 1/(1+exp(Qdiff/expT));
+        else
+            p = 1/(1+exp(Qdiff/expT));
+        end
+        
+        x = rand(1);
+        if x < p
+            currentSeq = newSampleSeq;
+            currentQuality = newQuality;
+            currentR = newR;
+            signRate = blockSignRate(targetR, newR, blocks, newBlock);
+            
+            if debugMode == 1
+                %print
+            end
+        else
+           if debugMode
+               %print
+           end
+        end
+    end
+    
+    finalSeq = currentSeq;
+    finalQual = currentQuality;
+    finalR = currentR;
+    fianlSignRate = signRate;
+
+end
