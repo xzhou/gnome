11094f38d2401e38f2da934d63096da5da9d0bce
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2010-01-07 04:19:57 +0000

    optimize estimate R

--- genotypeRecomb/estimateR.m
+++ genotypeRecomb/estimateR.m
@@ -1,6 +1,5 @@
 function [r pA, counts] = estimateR(genotypeSeq)
     [m n] = size(genotypeSeq);
-    
     r = zeros(n, n);    %pairwise allele requency
     %allocate the data structure
     result = getCounts(genotypeSeq);
@@ -9,14 +8,18 @@ function [r pA, counts] = estimateR(genotypeSeq)
     counts = result.counts;
     
     for i = 1:n-1
-       for j = i+1:n
+       parfor j = i+1:n
             p1 = pA(i);
             p2 = pA(j);
             n3x3 = reshape(counts(i,j,:,:), 3, 3);
             x = mleR(p1, p2, n3x3);
             r(i,j) = x.r;
-            r(j,i) = x.r;   %TODO change it to parallelize
         end
+    end
+    
+    %vectorize loop for speed
+    for i = 1:n-1
+        r(i+1:n,i) = r(i,i+1:n);
     end
 end
 
