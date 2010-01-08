4d84f6ed965397d08a062ee3c8ea38d834482b42
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2010-01-08 21:12:01 +0000

    update

---
+++ trunk/genotypeRecomb/haplotype2genotype.m
@@ -0,0 +1,26 @@
+function [genotype] = haplotype2genotype(haplotypeSeq, majorAllele)
+    if nargin == 1
+        majorAllele = getMajorAllele(haplotypeSeq);
+    end
+    
+    [m n] = size(haplotypeSeq);
+    nIndividual = round(m/2);
+    
+    genotype = zeros(nIndividual, n);
+    for i = 1:nIndividual           
+       for j = 1:n
+           a = haplotypeSeq(i, j);%the first allele
+           b = haplotypeSeq(i+1, j);%the next allele
+           m = majorAllele(j);%the major allele
+
+           %convert to genotype
+           if (a == m && b == m)
+               genotype(i,j) = 0;
+           elseif(a ~= m && b ~= m)
+               genotype(i,j) = 2;
+           else
+               genotype(i,j) = 1;
+           end
+       end
+    end
+end
