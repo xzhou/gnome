84b901cfbbd9750f83483a8bcd8c440155e52ad2
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2010-01-11 15:36:07 +0000

    add functions

---
+++ genotypeRecomb/checkEstimateR.m
@@ -0,0 +1,23 @@
+%in this file, we will check why I can not calculate r value correctly in
+%matlab
+cd '/home/xzhou/research_linux/gnome/workspace/data/HAPMAP';
+
+%=========configuration begin====================
+fastaFile = 'hapmap_chr7_80SNP_CEU_haplotype.fasta';
+%=========configuration end======================
+
+
+%calculat real R
+all = readSeq4(fastaFile);
+realR = calcR(all);
+alleleMapping = getMajorAllele(all);
+[singleAlleleFrequency c] = GnomeCalculator.getSingleAlleleFreq(all, alleleMapping);
+singleAlleleFrequency = singleAlleleFrequency';
+
+%estimate R from genotype
+genotypeAll = genotypeHelpFuncs.readGenotypeFromFasta(fastaFile);
+singleStat = getCounts(genotypeAll);
+[totalR pA counts] = estimateR(genotypeAll);
+
+
+%r from R 
