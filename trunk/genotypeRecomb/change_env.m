06237cb5499a4d029b45a1c3df012915fa30a550
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2010-01-20 19:23:43 +0000

    update

--- trunk/genotypeRecomb/change_env.m
+++ trunk/genotypeRecomb/change_env.m
@@ -1,10 +1,25 @@
 function [] = change_env()
 %CHANGE_ENV will handle all environmental problems
-currentDir = pwd;
-
-
-
-
-
+    path = pwd;
+    if start_with(path, '/home/xzhou')
+        disp 'running on xzhou.info'
+        addpath '/home/xzhou/research_linux/gnome/bioWorkspace/genomeprj/genotypeRecomb'
+        addpath '/home/xzhou/research_linux/gnome/bioWorkspace/genomeprj/hBlockRecombo'
+        addpath '~/research_linux/gnome/bioWorkspace/genomeprj/data/1500DataAnalysis'
+        cd '/home/xzhou/research_linux/gnome/bioWorkspace/genomeprj/data/1500DataAnalysis/WTCCC1/TPED'      
+    elseif start_with(path, '/u')
+        disp 'running on hulk.cs.indiana.edu'
+    elseif start_with(path, 'D:')
+        disp 'running on Windows, please update th path here :)'
+    end
 end
 
+function [result] = start_with(str1, str2)
+    [m len] = size(str2);
+    x = ((str1(1:len) == str2) == 0);
+    if sum(x) > 0
+        result = 0;
+    else
+        result = 1;
+    end
+end
