2528849f920ed9fab1f266ecabce65b9506f5c0f
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2010-02-02 18:04:01 +0000

    add get PhaseSeq.m

---
+++ trunk/genotypeRecomb/getPhaseSeq.m
@@ -0,0 +1,15 @@
+function [phasedSeq] = getPhaseSeq(id, fastaSeq)
+    [fm fn] = size(fastaSeq);
+    [im in] = size(id);
+    
+    selectedIndex = [];
+    
+    for i = 1:im
+        idName = id(i);
+        for j = 1:fm
+            
+            
+        end
+    end
+    
+end
