51a2806b19800244245228825c90ca2906ab4ef9
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2010-02-03 01:26:37 +0000

    update

--- genotypeRecomb/getPhaseSeq.m
+++ genotypeRecomb/getPhaseSeq.m
@@ -2,14 +2,21 @@ function [phasedSeq] = getPhaseSeq(id, fastaSeq)
     [fm fn] = size(fastaSeq);
     [im in] = size(id);
     
-    selectedIndex = [];
-    
+    phasedSeq = [];
     for i = 1:im
-        idName = id(i);
+        idName = id{i};
+        flag = 0;
         for j = 1:fm
-            
-            
+            fastaIdName = fastaSeq(j,1).Header;
+            k = findstr(idName, fastaIdName);
+            if ~isempty(k)
+                phasedSeq = [phasedSeq; fastaSeq(j,1); fastaSeq(j+1,1)];
+                flag = 1;
+                break;
+            end
+        end
+        if flag == 0
+            fprintf(1, '%s can not find\n', idName);
         end
     end
-    
 end
