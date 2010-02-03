727305b4f477b16fc82ac5a5555be5174d82b35a
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2010-02-03 16:39:46 +0000

    update

--- trunk/genotypeRecomb/analysisPhasedCaseRef.m
+++ trunk/genotypeRecomb/analysisPhasedCaseRef.m
@@ -1,14 +1,18 @@
-function [caseRef, coverRate] = analysisPhasedCaseRef(intSeq1, intSeq2, blocks)
+function [caseRef, coverRate, seq1Info, seq2Info] = analysisPhasedCaseRef(intSeq1, intSeq2, blocks)
+    [nseq, tmp] = size(intSeq1);
     seq1Info = getSeqFreqInfo(intSeq1, blocks);
     seq2Info = getSeqFreqInfo(intSeq2, blocks);
     [caseRef] = compareCaseRef(seq1Info, seq2Info, blocks);
-        
+    
     [nBlocks, n] = size(blocks);
-    coverRate = zeros(nBlocks, 1);
+    coverRate.typeCoverate = 0.0;   %number of hyplotype shared
+    coverRate.seqCoverate = 0.0;    %number sequence shared
+    coverRate(3).typeCoverate = 0.0;
     for i = 1:nBlocks
         [commonBlock, tmp] = size(caseRef{i,1});
         [caseNBlock, tmp] = size(seq1Info{i,1}.uniqueBlock);
-        coverRate(i,1) = commonBlock*1.0/caseNBlock;
+        coverRate(i).typeCoverate = commonBlock*1.0/caseNBlock;
+        coverRate(i).seqCoverate = sum(caseRef{1,1}(:,3))/nseq;
     end
 end
 
