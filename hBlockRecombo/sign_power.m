0cd17271585971218b798a6ea53e1a5f088e2e93
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2010-03-08 16:49:12 +0000

    update

--- hBlockRecombo/sign_power.m
+++ hBlockRecombo/sign_power.m
@@ -10,7 +10,7 @@ function [caseTr, refTr] = sign_power(hapSeq, nCase, nRef, trial)
 
 uniqueHapSeq = unique(hapSeq, 'rows');
 
-[m, n] = size(uniqueHapSeq);
+[m, nSnps] = size(uniqueHapSeq);
 
 totalSample = nCase + nRef;
 if totalSample > m
@@ -30,44 +30,26 @@ caseR(isnan(caseR)) = 0;%invariant sites
 refR(isnan(refR)) = 0;%invariant sites
 
 nSeg = 10; %divide 1-100 into 10 segment
-caseTr = zeros(nSeg, nCase, trial);
-refTr = zeros(nSeg, nRef, trial);
+caseTr = zeros(nSeg, trial, nCase);
+refTr = zeros(nSeg, trial, nRef);
 
 %calculate power
 for i = 1:nSeg
     fprintf(1, '%d\n', i);
     p = 0.1 * i;
-    for j = 1:nCase
-        Y = caseSeq(j,:);
-        caseTr(i,j, :) = getTr_P_N(Y, caseR, refR, p, trial);
+    for j = 1:trial
+        pMask = getMaskP(nSnps, p);
+        pCaseR = caseR .* pMask;
+        for k = 1:nCase
+            Yc = caseSeq(k,:);
+            Tr = getTr(Yc, pCaseR, refR);
+            caseTr(i, j, k) = Tr;
+        end
+        for k = 1:nRef
+            Yr = refSeq(k, :);
+            Tr = getTr(Yr, pCaseR, refR);
+            refTr(i, j, k) = Tr;
+        end
     end
-    for j = 1:nRef
-        Y = refSeq(j,:);
-        refTr(i,j, :) = getTr_P_N(Y, caseR, refR, p, trial);
-    end
 end
-
-end
-
-function [Tr] = getTr_P_N(Y, caseR, refR, p, trial)
-%calculate the power of Y using 100p percent signs, try trial number of
-%times
-Tr = zeros(trial, 1);
-for i = 1:trial
-    Tr(i) = getTr_P(Y, caseR, refR, p);
-end
-end
-
-function [tr] = getTr_P(Y, caseR, refR, p)
-%randomly inverse 1-p signs
-[m n] = size(caseR);
-mask = ones(m, n);
-for i = 1:m-1
-    x = (rand(m - i, 1) < p)*2 - 1;
-    mask(i, i+1:m) = x;
-end
-mask = copyUpperToLower(mask);
-caseR = caseR .* mask;
-tr = getTr(Y, caseR, refR);
-tr = tr/sqrt(n*(n-1)/2);
 end
