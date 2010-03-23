714d32da7d345327718a62244259f5dc48993be9
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2010-03-23 20:24:26 +0000

    add homer's attack

--- hBlockRecombo/sign_power.m
+++ hBlockRecombo/sign_power.m
@@ -1,4 +1,4 @@
-function [caseTr, refTr, testTr] = sign_power(hapSeq, nCase, nRef, nTest, trial, useEstR)
+function [caseTr, refTr, testTr, caseTp, refTp, testTp] = sign_power(hapSeq, nCase, nRef, nTest, trial, useEstR)
 %explore the relationship between sign and power. Previously we though some
 %sign has larger power. And even 10% sign has significant power. I think
 %it's now stable since some pair has negative effects to the power,
@@ -8,37 +8,74 @@ function [caseTr, refTr, testTr] = sign_power(hapSeq, nCase, nRef, nTest, trial,
 %@hapSeq is the 0-1 encoding of hap snps sequence
 %@caseSize is the population size of case and reference
 
-uniqueHapSeq = unique(hapSeq, 'rows');
+%uniqueHapSeq = unique(hapSeq, 'rows');
 
-[m, nSnps] = size(uniqueHapSeq);
+[m, nSnps] = size(hapSeq);
 
 totalSample = nCase + nRef + nTest;
-if totalSample > m
-    e = MException('signPower:notEnoughUniqueSeq', 'xzhou: not enough unique sequence');
+
+if 2*totalSample > m
+    e = MException('signPower:notEnoughUniqueSeq', 'xzhou:not enough unique sequence');
     throw(e);
 end
 
-idx = randsample(m, totalSample);
+%idx of individuals
+idx = randsample(m/2, totalSample);
+idxCase = sort([idx(1:nCase)*2; idx(1:nCase)*2-1]);
+idxRef = sort([idx(nCase+1:nCase+nRef)*2; idx(nCase+1:nCase+nRef)*2-1]);
+idxTest = sort([idx(nCase+nRef+1:end)*2; idx(nCase+nRef+1:end)*2-1]);
 
-caseSeq = uniqueHapSeq(idx(1:nCase), :);
-refSeq = uniqueHapSeq(idx(nCase+1:nCase+nRef), :);
-testSeq = uniqueHapSeq(idx(nCase+nRef+1:end), :);
+caseSeq = hapSeq(idxCase, :);
+refSeq = hapSeq(idxRef, :);
+testSeq = hapSeq(idxTest, :);
+
+%convert hapseq to genoseq
+caseGenoSeq = combineHapSeq(caseSeq);
+refGenoSeq = combineHapSeq(refSeq);
+testGenoSeq = combineHapSeq(testSeq);
 
 if useEstR == 0
     caseR = corrcoef(caseSeq);
     refR = corrcoef(refSeq);
 else
-    caseR = hapSeqEstR(caseSeq);
-    refR = hapSeqEstR(refSeq);
+    caseR = estimateR(caseGenoSeq);
+    refR = estimateR(refGenoSeq);
 end
 
 caseR(isnan(caseR)) = 0;%invariant sites
 refR(isnan(refR)) = 0;%invariant sites
 
+caseP = sum(caseSeq)/nCase;
+refP = sum(refSeq)/nRef;
+
 nSeg = 10; %divide 1-100 into 10 segment
-caseTr = zeros(nSeg, trial, nCase);
-refTr = zeros(nSeg, trial, nRef);
-testTr = zeros(nSeg, trial, nTest);
+caseTr = zeros(nSeg, trial, 2*nCase);
+refTr = zeros(nSeg, trial, 2*nRef);
+testTr = zeros(nSeg, trial, 2*nTest);
+
+caseTp = zeros(nCase, 1);
+refTp = zeros(nRef, 1);
+testTp = zeros(nTest, 1);
+
+%calculate Homer's attack
+%case
+for k = 1:nCase
+    Yk = caseGenoSeq(k, :);
+    Tp = getTp(Yk, caseP, refP);
+    caseTp(k) = Tp;
+end
+%ref
+for k = 1:nRef
+    Yk = refGenoSeq(k, :);
+    Tp = getTp(Yk, caseP, refP);
+    refTp(k) = Tp;
+end
+%test
+for k = 1:nTest
+    Yk = testGenoSeq(k, :);
+    Tp = getTp(Yk, caseP, refP);
+    testTp(k) = Tp;
+end
 
 %calculate power
 %for different recover rate
@@ -51,17 +88,17 @@ for i = 1:nSeg
         pMask = getMaskP(nSnps, p);
         pCaseR = caseR .* pMask;
         %for each individual
-        for k = 1:nCase
+        for k = 1:2*nCase
             Yc = caseSeq(k,:);
             Tr = getTr(Yc, pCaseR, refR);
             caseTr(i, j, k) = Tr;
         end
-        for k = 1:nRef
+        for k = 1:2*nRef
             Yr = refSeq(k, :);
             Tr = getTr(Yr, pCaseR, refR);
             refTr(i, j, k) = Tr;
         end
-        for k = 1:nTest
+        for k = 1:2*nTest
             Yt = testSeq(k, :);
             Tt = getTr(Yt, pCaseR, refR);
             testTr(i, j, k) = Tt;
