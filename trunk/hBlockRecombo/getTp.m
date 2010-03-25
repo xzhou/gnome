65489b0c961e3695be4c13a857ae661ea92cd329
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2010-03-25 01:33:36 +0000

    update

--- trunk/hBlockRecombo/getTp.m
+++ trunk/hBlockRecombo/getTp.m
@@ -1,5 +1,5 @@
-function [tp] = getTp(Y, pM0, pM1)
+function [tp] = getTp(Y, pM1, pM0)
 %calculate Tp value using Homer's attack
 p = (-(Y-1)+1)/2;
-tp = sum(abs(p - pM1) - abs(p - pM0));
+tp = sum(abs(p - pM0) - abs(p - pM1));
 end
