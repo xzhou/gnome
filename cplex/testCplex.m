d38d0688886a666ee82c333b8794c34fa6e16e01
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2010-09-08 20:15:09 +0000

    save cplexmilp

--- cplex/testCplex.m
+++ cplex/testCplex.m
@@ -16,17 +16,28 @@ function testCplex()
     lb = [0, 0]';
     ub = [5.5, 5.5]';
     
-    rhs = 9.3;
+    rhs = [9.3];
     
     cplex.Model.name = 'test mip';
     cplex.Model.lb = lb;
+    cplex.Model.obj = obj;
     cplex.Model.ub = ub;
     cplex.Model.A = A;
     cplex.Model.rhs = rhs;
-    
-    cplex.Model.ctype = 'II';
+    cplex.Model.lhs = [-inf];
     cplex.Model.sense = 'maximize';
-    cplex.Param.mip.pool.relgap.Cur = 0.1; 
-    cplex.populate();
     
+    %optional
+    cplex.Model.ctype = 'II';
+    cplex.Param.mip.pool.intensity.Cur = 4;
+    %cplex.Param.mip.limits.populate.Cur = 3;
+    cplex.Param.mip.pool.relgap.Cur = 0;
+    cplex.populate();
+    %control searching strategy
+    numsoln = size (cplex.Solution.pool.solution, 1);
+    
+    for i = 1:numsoln
+      %fprintf ('%8d  %13g', i, cplex.Solution.pool.solution(i).objval);
+      cplex.Solution.pool.solution(i).x
+   end
 end
