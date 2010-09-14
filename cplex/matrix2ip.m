47cfee5c98c85ae8e8046904414578e2ba54757a
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2010-09-14 04:04:11 +0000

    update

--- cplex/matrix2ip.m
+++ cplex/matrix2ip.m
@@ -1,9 +1,11 @@
 function matrix2ip(M)
 if nargin == 0
     M = [0 0 0;0 1 1; 1 0 1; 1 1 0; 1 1 0];
-    M = [0 0 0 1;0 1 1 0; 1 0 1 0; 1 1 0 0; 1 1 0 0];
+    M = [0 0 0 1 1;0 1 1 0 0; 1 0 1 0 1; 1 1 0 0 0; 1 1 0 0 1]
 end
  
+%M = M(1:2, 1:end);
+    
 [m n] = size(M);
 
 %single allele frequency
@@ -17,7 +19,7 @@ nPair = nchoosek(n,2);  %the number of pairs of pairwise counts
 nAuxVar = m*nPair;      %the number of auxiliary variables, each pair of columns has m pair
 nVar = m*n + nAuxVar;   %the number of variables including the Aux variables
 
-%target function
+%target as an feasibility problem
 f = zeros(nVar, 1);
 
 Aeq = zeros(n+nPair, nVar);   % n P constraints, 
@@ -42,6 +44,8 @@ for i = 1:nPair
 end
 Aeq((n+1):end, (m*n+1):end) = CC;
 beq = [beq; c00];
+
+lhs = beq;
 
 % bound Aux variables
 
@@ -68,28 +72,54 @@ for j = 1:(n-1)
     end
 end
 
-options = cplexoptimset;
-options.Diagnostics = 'on';
-options.SolnPoolPop = 2;
-options.SolnPoolIntensity = 4;
 
+A = [Aeq; Aineq];
+lhs = [lhs; ones(size(bineq))*-inf];
+rhs = [beq; bineq]; %relax constraints but does not change the result
 
 lb = zeros(nVar, 1);    %binary constraints >= 0
 ub = ones(nVar, 1);     %binary constrinats <= 1
 ctype = [];
 for i = 1:nVar
-    ctype = [ctype, 'I'];
+    ctype = [ctype, 'B'];
 end
 
-options = cplexoptimset;
-options.DIagnostics = 'on';
-options.SolnPoolPop = 2;
+%options = cplexoptimset;
+%options.DIagnostics = 'on';
+%options.SolnPoolPop = 2;
 
 %formalize cplex solver
-[x, fval, exitflag, output] = cplexmilp ...
-    (f, Aineq, bineq, Aeq, beq, [], [], [], lb, ub, ctype, [], options);
+%[x, fval, exitflag, output] = cplexmilp ...
+    %(f, Aineq, bineq, Aeq, beq, [], [], [], lb, ub, ctype, [], options);
 
-reversedMatrix = reshape(x(1:(m*n)), m, n)
+%reversedMatrix = reshape(x(1:(m*n)), m, n)
+
+%build mip model
+cplex = Cplex();
+
+cplex.Model.obj = f;
+cplex.Model.A = A;
+cplex.Model.rhs = rhs;
+cplex.Model.lhs = lhs;
+cplex.Model.name = 'reverse';
+cplex.Model.lb = lb;
+cplex.Model.ub = ub;
+cplex.Model.sense = 'maximize';
+cplex.Model.ctype = ctype;
+cplex.writeModel('reverse.lp');
+
+%options
+cplex.Param.mip.pool.intensity.Cur = 4;
+
+cplex.populate();
+
+numsoln = size (cplex.Solution.pool.solution, 1)
+
+for i = 1:numsoln
+    fprintf(1, 'solutions %d\n', i);
+    %cplex.Solution.pool.solution(i).x
+    reverseM = reshape(cplex.Solution.pool.solution(i).x(1:(m*n)), m, n)
+end
 
 end
 
