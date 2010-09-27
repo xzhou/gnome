b72a4d5cbf12d82fbcd9119a4d27aa5fd75b34f6
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2010-09-27 17:37:10 +0000

    update

--- trunk/MatrixRecover/MatrixIndex.h
+++ trunk/MatrixRecover/MatrixIndex.h
@@ -11,13 +11,19 @@
 #define MATRIXINDEX_H_
 
 #include <list>
+#include <map>
+#include <set>
+#include <string>
+using namespace std;
 
 class MatrixIndex {
-	list<string> slnPool;	//a MD5 solution list
+	set<string> solutionPool;	//this is a solution MD5 code
 public:
 	MatrixIndex();
-	int addToPool(int **M, int m, int n);
-	bool existInPool(int **M, int m, int n);
+	int addToPool(int *M, int m, int n);
+	bool existInPool(int *M, int m, int n);
+	string m2s(int *M, int m, int n);
+	string s2signature(string s);
 	virtual ~MatrixIndex();
 };
 
