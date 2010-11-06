396e503b5b21e0019934c4e848af4eabb6251a6d
zhou.xiaoyong <zhou.xiaoyong@f091b60a-4754-11de-82b9-170f4fd74ac7>
2010-11-06 00:25:50 +0000

    add fix and solve, waiting for debug

--- trunk/MatrixRecover/Solver.cpp
+++ trunk/MatrixRecover/Solver.cpp
@@ -214,7 +214,7 @@ SlnPool * Solver::solveAndFilter(SnpMatrix &M){
 				}
 			}
 		}
-		//cout<<"pairwise constraint added"<<endl;
+
 		model.add(con);
 		//export the model for verification
 		IloCplex cplex(model);
@@ -249,6 +249,157 @@ SlnPool * Solver::solveAndFilter(SnpMatrix &M){
 		cerr << "concert exception caught: " << e << endl;
 	}
 	c2.close();
+	env.end();
+	return sp;
+}
+
+/*
+ *	This method try to prove that given a solution,
+ *  the attacker can not fix one of them using fix and solve method
+ *	This
+ */
+SlnPool* Solver::fixAndSolve(SnpMatrix &M, int fixRow){
+	int m = M.nInd;
+	int n = M.nSnp;
+
+	int *fixVar = M.M[fixRow];
+
+	int nPair = nchoosek(n, 2);		//number of pairwise constraints
+	int nAuxVar = m*nPair;			//auxiliary variable
+	int nFixVar = m*n;				//fixing one row we need this
+	int nVar = m*n + nFixVar + nAuxVar;
+
+	int numSolution = -1;
+	//int nRealSln = 0;
+	SlnPool *sp = new SlnPool();
+
+	IloEnv env;
+	ofstream c2("c2.log");
+	env.setOut(c2);
+
+	try{
+		IloModel model(env);
+
+		//initialize the obj function
+		IloNumArray obj(env);
+		IloNumVarArray x(env);
+
+		//initialize the obj function
+		for (int i = 0; i < nVar; i ++){
+			obj.add(0);
+			x.add(IloNumVar(env, 0, 1, ILOINT));
+		}
+		if (obj.getSize() != nVar) {
+			cerr<<"initializing the target"<<endl;
+		}
+		if (x.getSize() != obj.getSize()){
+			cerr<<"X:"<<x.getSize()<<" obj: "<<obj.getSize()<<endl;
+		}
+
+		//create objective as 0
+		model.add(IloMaximize(env, 0));
+
+		IloRangeArray con(env);
+		//add p constraints
+		for (int j = 0; j < n; j ++){
+			IloExpr pcons(env);
+			for(int i = 0; i < m;  i ++){
+				pcons += x[j*m + i] * 1;
+			}
+			con.add(M.P[j] <= pcons <= M.P[j]);
+		}
+		//cout<<"p constraints added"<<endl;
+		//add R constraints, e.g. c00 constraints
+		int base = m*n;
+		for(int i = 0; i < nPair; i ++){
+			int begin = m*i;
+			int end = m*(i+1)-1;
+
+			IloExpr rcons(env);
+			for(int j = begin; j <= end; j ++){
+				rcons += x[base + j] * 1;
+			}
+			con.add(M.LR[i] <= rcons <= M.LR[i]);
+		}
+		//cout<<"r constraints added"<<endl;
+
+		//add x_ij ~ axu_ijk constraints
+		for (int j = 0; j < n-1; j ++) {
+			for (int k = j+1; k < n; k ++ ) {
+				for (int i = 0; i < m; i ++ ) {
+					int d1 = j*m + i;
+					int d2 = k*m + i;
+					int dd_relative = calcdd(i, j, k, m, n);
+					int dd = dd_relative + m*n - 1;
+					//cout<<d1<<" "<<d2<<" "<<dd_relative<<"/"<<dd<<endl;
+					IloExpr auxConsTop(env);
+					auxConsTop = - x[d1] - x[d2] + 2 * x[dd];
+					con.add(-INFINITY <= auxConsTop <= 0);
+
+					IloExpr auxConsBtn(env);
+					auxConsBtn =   x[d1] + x[d2] - 2 * x[dd];
+					con.add(-INFINITY <= auxConsBtn <= 1);
+				}
+			}
+		}
+		//adding fixing constraints
+		int fixVarBeginIndex = m*n + nAuxVar;	//beginning of aux variable
+		for(int i = 0; i < m; i ++){
+			for(int j = 0; j < n; j ++){
+				int x_ij = j*m + i;	//index variable
+				int fixVarIdx = fixVarBeginIndex + x_ij;
+
+				IloExpr fixVarCons1(env);
+				fixVarCons1 = x[fixVarIdx] - x[x_ij] - fixVar[j];
+				con.add(-INFINITY <= fixVarCons1 <= 0);
+
+				IloExpr fixVarCons2(env);
+				fixVarCons2 = x[fixVarIdx] + x[x_ij] + fixVar[j];
+				con.add(-INFINITY <= fixVarCons2 <= 2);
+
+				IloExpr fixVarCons3(env);
+				fixVarCons3 = x[fixVarIdx] + x[x_ij] - fixVar[j];
+				con.add(0 <= fixVarCons3 <= INFINITY);
+
+				IloExpr fixVarCons4(env);
+				fixVarCons4 = x[fixVarIdx] - x[x_ij] + fixVar[j];
+				con.add(0 <= fixVarCons4 <= INFINITY);
+			}
+		}
+
+		//constraints for fixing variable
+		for(int i = 0; i < m; i ++){
+			IloExpr fcons(env);
+			for(int j = 0; i < m; j ++){
+				int idx = fixVarBeginIndex + j*m+i;
+				fcons += x[idx]*1;
+			}
+			con.add(0<=fcons<=n);
+		}
+		model.add(con);
+
+		IloCplex cplex(model);
+		cplex.exportModel("fixSolve.lp");
+
+		//debug
+		exit(0);
+		//set parameters before populate solutions
+		cplex.setParam(IloCplex::SolnPoolIntensity, 4);	//enum all solutions
+		cplex.setParam(IloCplex::SolnPoolGap, 0);
+		IloCplex::Callback mycallback = cplex.use(SolutionFilterCallback(env, x, m, n, sp));
+		cplex.populate();
+
+		//check the solutions
+		numSolution = cplex.getSolnPoolNsolns();
+		env.out()<<"the solution pool contains "<<numSolution<<" solutions"<<endl;
+		mycallback.end();
+		env.end();
+	}
+	catch (IloException &e)
+	{
+		cerr<<"concert exception caught: "<< e << endl;
+	}
+
 	env.end();
 	return sp;
 }
